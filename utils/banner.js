0e2a09fb962673907fede5e4017e8e59ae85a48a
Hardeep Singh <ihardeepdhaliwal@gmail.com>
2025-01-28 17:45:29 -0800

    Add files via upload

---
+++ utils/banner.js
@@ -0,0 +1,21 @@
+const banner = `
+
+██████╗ ███████╗███████╗██████╗  
+██╔══██╗██╔════╝██╔════╝██╔══██╗ 
+██║  ██║█████╗  █████╗  ██████╔╝ 
+██║  ██║██╔══╝  ██╔══╝  ██╔═══╝  
+██████╔╝███████╗███████╗██║      
+╚═════╝ ╚══════╝╚══════╝╚═╝      
+
+
+    === Running LitasBot CLI Version ===
+** ====================================== **
+*    This script is created for free use   *
+*  Do not sell or distribute it for profit *
+** ====================================== **
+
+
+* Author: @soramindAI                                     
+* Github Link: github.com/hardeeps647       
+                 `
+export default banner;
